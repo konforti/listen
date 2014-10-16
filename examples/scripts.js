5e0cf1b02372737994ae114c44a6e77782a41ef8
Raz Konforti <konforti@gmail.com>
2014-10-16 16:54:43 +0300

    wp code

--- examples/scripts.js
+++ examples/scripts.js
@@ -1,50 +1,76 @@
 (function() {
+  var wp = document.getElementById( 'wp' ).checked;
   var embedScript = '<script>(function(d,s,id){var js, fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="http://konforti.net/listen/listen.js";fjs.parentNode.insertBefore(js,fjs);})(document,"script","listen-embed");</script>';
 //  var embedScript = '<script async src="http://konforti.net/listen/listen.js"></script>';
 
-// Update embed code.
-  var embedCode = function () {
+  /**
+   * Update embed code.
+   */
+  var updateCode = function () {
     var text = document.getElementById( 'text' ).value;
     var file = document.getElementById( 'file' ).value;
     var start = document.getElementById( 'start' ).value;
     var end = document.getElementById( 'end' ).value;
-    document.getElementById( 'code' ).value = '<span class="listen-node" data-src="' + file + '" data-start="' + start + '" data-end="' + end + '">' + text + '</span>' + embedScript;
+    var code = '';
+
+    if ( wp ) {
+      code = '[listen src="' + file + '" start="' + start + '" end="' + end + '"]' + text + '[/listen]';
+    }
+    else {
+      code = '<span class="listen-node" data-src="' + file + '" data-start="' + start + '" data-end="' + end + '">' + text + '</span>' + embedScript;
+    }
+
+    document.getElementById( 'code' ).value = code;
   }
 
+  /**
+   * Update audio player.
+   */
   var updatePlayer = function() {
     var file = document.getElementById( 'file' ).value;
     document.getElementById( 'player' ).setAttribute( "src", file );
   }
 
-// On changes.
+  // On changes.
   document.addEventListener( 'paste', function ( e ) {
     if ( e.target.type === 'text' ) {
-      embedCode();
-    }
-    if (e.target.id === 'audio') {
-      updatePlayer();
-    }
-  } );
-  document.addEventListener( 'keyup', function ( e ) {
-    if ( e.target.type === 'text' ) {
-      embedCode();
+      updateCode();
     }
     if (e.target.id === 'audio') {
       updatePlayer();
     }
   } );
 
+  // On keyup.
+  document.addEventListener( 'keyup', function ( e ) {
+    if ( e.target.type === 'text' ) {
+      updateCode();
+    }
+    if (e.target.id === 'audio') {
+      updatePlayer();
+    }
+  } );
+
+  // On click
   document.addEventListener( 'click', function ( e ) {
     if ( e.target.id == 'start-btn' ) {
       document.getElementById( 'start' ).value = document.getElementById( 'player' ).currentTime.toFixed();
-      embedCode();
+      updateCode();
     }
     else if ( e.target.id == 'end-btn' ) {
       document.getElementById( 'end' ).value = document.getElementById( 'player' ).currentTime.toFixed();
-      embedCode();
+      updateCode();
     }
   } );
 
-  embedCode();
+ // On change
+  document.addEventListener( 'change', function ( e ) {
+    if ( e.target.id == 'wp' ) {
+      wp = e.target.checked;
+      updateCode();
+    }
+  } );
+
+  updateCode();
   updatePlayer();
 })();
