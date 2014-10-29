39d95447a6ec84760e3c223f811b6c3b7de8b3fb
Raz Konforti <konforti@gmail.com>
2014-10-29 08:36:15 +0200

    -

--- listen.js
+++ listen.js
@@ -1,75 +1,79 @@
+// listen.js
+// Copyright (c) 2014 Konforti
+// https://github.com/konforti/listen
+
 (function () {
   "use strict";
 
   var
       name = "listen-node",
-      els = document.getElementsByClassName( name ),
-      a, b, c;
+      elements = document.getElementsByClassName( name ),
+      aud, elm, icn;
 
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".listen-node {display: inline-block; background:rgba(0, 0, 0, 0.05); padding: 1px 5px; border-radius:4px; cursor: pointer;} .listen-node i {font-size: 0.7em; border: 0.5em solid transparent; border-left: 0.75em solid; display: inline-block; margin-left: 5px;} .listen-node .playing { border: 0; border-left: 0.75em double; border-right: 0.5em solid transparent; height: 1em;}";
   document.getElementsByTagName("head")[0].appendChild(css);
 
-  for ( var i = 0; b = els[i]; ++i ) {
-    a = document.createElement( 'audio' );
-    c = document.createElement( 'i' );
+  for ( var i = 0; elm = elements[i]; ++i ) {
+    aud = document.createElement( 'audio' );
+    icn = document.createElement( 'i' );
 
-    a.src = b.getAttribute( "data-src" );
-    a.setAttribute( "data-playing", "false" );
+    aud.src = elm.getAttribute( "data-src" );
+    aud.setAttribute( "data-playing", "false" );
 
-    b.id = name + "-" + i;
-    b.insertBefore( c, b.firstChild );
-    b.appendChild( a );
+    elm.id = name + "-" + i;
+    elm.insertBefore( icn, elm.firstChild );
+    elm.appendChild( aud );
   }
 
-  function _play( a, c ) {
-    a.play();
-    a.setAttribute( "data-playing", "true" );
-    c.classList.add("playing");
+  function _play( aud, icn ) {
+    aud.play();
+    aud.setAttribute( "data-playing", "true" );
+    icn.classList.add("playing");
   }
 
-  function _pause( a, c ) {
-    a.pause();
-    a.setAttribute( "data-playing", "false" );
-    c.classList.remove("playing");
+  function _pause( aud, icn ) {
+    aud.pause();
+    aud.setAttribute( "data-playing", "false" );
+    icn.classList.remove("playing");
   }
 
   document.addEventListener( 'click', function ( e ) {
-    var a, b, c;
+    var aud, elm, icn;
     if ( e.target.className === name ) {
-      a = e.target.children[1];
-      b = e.target;
-      c = e.target.children[0];
+      aud = e.target.children[1];
+      elm = e.target;
+      icn = e.target.children[0];
     }
     else if ( e.target.parentElement && e.target.parentElement.className === name ) {
-      a = e.target.parentElement.children[1];
-      b = e.target.parentElement;
-      c = e.target;
+      aud = e.target.parentElement.children[1];
+      elm = e.target.parentElement;
+      icn = e.target;
     }
 
-    if (a && b && c) {
-      a.srt = parseInt( b.getAttribute( 'data-start' ) ) || 0;
-      a.end = parseInt( b.getAttribute( 'data-end' ) ) || a.duration;
+    if (aud && elm && icn) {
+      aud.srt = parseInt( elm.getAttribute( 'data-start' ) ) || 0;
+      aud.end = parseInt( elm.getAttribute( 'data-end' ) ) || aud.duration;
 
-      if ( a && a.getAttribute( "data-playing" ) === "false" ) {
-        if ( a.srt > a.currentTime || a.end < a.currentTime ) {
-          a.currentTime = a.srt;
+      if ( aud && aud.getAttribute( "data-playing" ) === "false" ) {
+        if ( aud.srt > aud.currentTime || aud.end < aud.currentTime ) {
+          aud.currentTime = aud.srt;
         }
-        _play( a, c );
+        _play( aud, icn );
       }
       else {
-        _pause( a, c );
+        _pause( aud, icn );
       }
 
       (function loop() {
         var d = requestAnimationFrame( loop );
-        var percent = (((a.currentTime - a.srt) * 100) / (a.end - a.srt));
+        var percent = (((aud.currentTime - aud.srt) * 100) / (aud.end - aud.srt));
         percent = percent < 100 ? percent : 100;
-        b.style.background = "linear-gradient(to right, rgba(0, 0, 0, 0.1)" + percent + "%, rgba(0, 0, 0, 0.05)" + percent + "%)";
+        elm.style.background = "linear-gradient(to right, rgba(0, 0, 0, 0.1)" + percent + "%, rgba(0, 0, 0, 0.05)" + percent + "%)";
 
-        if ( a.end < a.currentTime ) {
-          _pause( a, c );
+        if ( aud.end < aud.currentTime ) {
+          _pause( aud, icn );
           cancelAnimationFrame( d );
         }
       })();
